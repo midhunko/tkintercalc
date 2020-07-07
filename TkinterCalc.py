5a86ec4f5824ecbd47d76272c68968d5d6f9c786
Midhun KO <midhunko1996@outlook.com>
2020-07-07 16:43:04 +0530

    Basics of Tkinter

--- TkinterCalc.py
+++ TkinterCalc.py
@@ -1,1 +1,11 @@
-  
+from tkinter import  *
+
+root = Tk()
+#creating a lable
+myLable1 = Label(root, text = "Hello World")
+myLable2 = Label(root, text = "How are u?")
+#moving it to the screen
+myLable1.grid(row = 0, column = 0)
+myLable2.grid(row = 1, column = 0)
+
+root.mainloop()
